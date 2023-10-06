8fb88c830daf0ab6e12e982aafff238f5f45855f
Anwar <anwar.28.8.1999@outlook.com>
2023-10-06 04:47:11 +0300

    task6 modify1: edit specifier

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,18 +2,18 @@
 
 /**
   * main - Entry point
-  * 
+  *
   * Description: This program display the size of differete type of data
   * Return: Always 0 (Success)
   */
 
 int main(void)
 {
-	printf("Size of a char: %c byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %lld byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %f byte(s)\n", sizeof(float));
+	printf("Size of a char: %u byte(s)\n", sizeof(char);
+	printf("Size of an int: %u byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %llu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %u byte(s)\n", sizeof(float));
 
 	return (0);
 }

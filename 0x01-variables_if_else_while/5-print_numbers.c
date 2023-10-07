bce326843b28c8df8e1aaf081a529148af7e0cf2
Anwar <anwar.28.8.1999@outlook.com>
2023-10-07 04:15:48 +0300

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Description: print numbers from 0 to 9
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}

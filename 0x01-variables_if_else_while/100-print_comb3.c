ff2348af50a48db02749a8a85c9739c61571a73d
Anwar <anwar.28.8.1999@outlook.com>
2023-10-07 05:18:04 +0300

    task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Despription: print the compination of two digits
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int n1 = 48, n2;
+
+	while (n1 <= 57)
+	{
+		n2 = n1 + 1;
+		while (n2 <= 57)
+		{
+			putchar(n1);
+			if (n2 != n1)
+			{
+				putchar(n2);
+				if (n1 < 56)
+				{
+					putchar(',');
+					putchar(' ');
+			
+				}
+			}
+			n2++;
+		}
+		n1++;
+	}
+	putchar('\n');
+	return (0);
+}

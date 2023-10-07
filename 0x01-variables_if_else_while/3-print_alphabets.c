d601b83ee6dfc82d60d7d14a000f44787f6643ca
Anwar <anwar.28.8.1999@outlook.com>
2023-10-07 03:30:14 +0300

    task3 modify1: correct output

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,15 +10,17 @@
 
 int main(void)
 {
-	char c = 'A';
+	char c = 'a', C = 'A';
 
 	while (c <= 'z')
 	{
 		putchar(c);
-
-		if (c == 'Z')
-			c += 6;
 		c++;
+	}
+	while (C <= 'Z')
+	{
+		putchar(C);
+		C++;
 	}
 	putchar('\n');
 	return (0);

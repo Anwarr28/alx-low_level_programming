7d1cbc713cd3f122bf5e4100b6b6e1a54646890b
Anwar <anwar.28.8.1999@outlook.com>
2023-10-11 02:33:22 +0300

    task 5 modify 1 : fix betty

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,8 +5,8 @@
   * print_sign - prints the sign of a number.
   * @n: the number to be checked.
   *
-  * Return: 1 if the number is greater than 0, 0 if it is zero 
-  		and -1 if less than 0.
+  * Return: 1 if the number is greater than 0, 0 if it is zero
+  * and -1 if less than 0.
   */
 
 int print_sign(int n)
@@ -16,7 +16,7 @@ int print_sign(int n)
 		_putchar('-');
 		return (-1);
 	}
-	else if ( n > 0)
+	else if (n > 0)
 	{
 		_putchar('+');
 		return (1);

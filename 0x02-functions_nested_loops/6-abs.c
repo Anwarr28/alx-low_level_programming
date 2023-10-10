e124ec0b4ad98d6a3ec0ca754797763209f7590f
Anwar <anwar.28.8.1999@outlook.com>
2023-10-11 02:40:15 +0300

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * _abs - compute the apsolute value of an integer.
+  * @n: the number to be computed.
+  *
+  * Return: the new number of type int.
+  */
+
+int _abs(int n)
+{
+	n = n * -1;
+	return (n);
+}

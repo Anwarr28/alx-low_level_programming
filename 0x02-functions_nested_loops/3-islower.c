87588d883c8697907861ce4b9cee1df9c9df528a
Anwar <anwar.28.8.1999@outlook.com>
2023-10-11 02:18:56 +0300

    task 3-4 modify1: fix error

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -10,8 +10,6 @@
 
 int _islower(int c)
 {
-	int c;
-
 	if (c <= 'z' && c >= 'a')
 		return (1);
 	else
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,8 +10,6 @@
 
 int _isalpha(int c)
 {
-	int c;
-
 	if (c <= 'z' && c >= 'a' || c <= 'Z' && c >= 'A')
 		return (1);
 	else

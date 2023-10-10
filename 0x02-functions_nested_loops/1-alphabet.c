3838c598de006683a75a3638c6f689d907c4b2dd
Anwar <anwar.28.8.1999@outlook.com>
2023-10-11 01:26:16 +0300

    task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * print_alphabet: function prints alphabet in lowercase letters.
+  *
+  * Retrun: return nothing
+  */
+
+void print_alphabet(void)
+{
+	char alpha = 'a';
+
+	for (; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+}

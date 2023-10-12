882f0e3e4c4947d7ef3a4d7855df8a3b61a88794
Anwar <anwar.28.8.1999@outlook.com>
2023-10-12 12:12:04 +0300

    fix 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,10 +7,20 @@
   * Return: 1 if th char is upper, 0 othrewise.
   */
 
-int _isupper(int c);
+int _isupper(int c)
 {
 	if (c <= 'Z' && c >= 'A')
 		return (1);
 	else
 		return (0);
+}
+int main(void)
+{
+    char c;
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
 }

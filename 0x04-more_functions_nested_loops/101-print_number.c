2901d240a507816575fe8b712feedfbd39e306ca
Anwar <anwar.28.8.1999@outlook.com>
2023-10-12 17:24:48 +0300

    fix 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-int num_length(int n);
 
 /**
   * print_number - prints an integer.
@@ -10,8 +9,6 @@ int num_length(int n);
 
 void print_number(int n)
 {
-	int len = num_length(n);
-	int j = 0, i = 1, a;
 	unsigned int num;
 
 	if (n < 0)
@@ -19,51 +16,8 @@ void print_number(int n)
 		_putchar('-');
 		num = -n;
 	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
 	else
-		num = n;
-	while ((len - 1) != 0)
-	{
-		i *= 10;
-		len--;
-	}
-	len = num_length(n);
-	while (j < len)
-	{
-		if (j < (len - 1))
-		{
-			a = num / i;
-			_putchar(a % 10 + '0');
-			i /= 10;
-		}
-		else
-			_putchar(num % 10 + '0');
-		j++;
-	}
-}
-
-/**
-  * num_length - compute a number length.
-  * @n: the number to be measured.
-  *
-  * Return: length of the number.
-  */
-
-int num_length(int n)
-{
-	int len = 1, i = 10;
-
-	if (n < 0)
-		n = -n;
-	while (1)
-	{
-		if (n / i > 0)
-		{
-			len++;
-			i *= 10;
-			continue;
-		}
-		else
-			break;
-	}
-	return (len);
+		_putchar(num % 10 + '0')
 }

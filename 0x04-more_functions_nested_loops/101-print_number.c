36c28a3da4184d850c891c4aa5a34dfefdaf06bf
Anwar <anwar.28.8.1999@outlook.com>
2023-10-12 17:31:27 +0300

    fix 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,15 +9,20 @@
 
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int num;
 
 	if (n < 0)
 	{
 		_putchar('-');
 		num = -n;
 	}
-	if ((num / 10) > 0)
-		print_number(num / 10);
 	else
-		_putchar(num % 10 + '0');
+	{
+		num = n;
+	}
+	if (num / 10)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
 }

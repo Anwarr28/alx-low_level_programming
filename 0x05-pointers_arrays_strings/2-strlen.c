e6c08bbd559ab248084225c6e7eba56b750a2174
Anwar <anwar.28.8.1999@outlook.com>
2023-10-17 02:31:46 +0300

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * _strlen - return the length of a string.
+  * @s: the string to be operated on.
+  *
+  * Return: the length of the string.
+  */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s)
+		len++;
+	return (len);
+}

941872e8af83043b2040c1a033c0e05f2e8cb86e
Anwar <anwar.28.8.1999@outlook.com>
2023-10-17 05:31:18 +0300

    task 6 modify1

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,12 +9,22 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i = 0, len = 0;
 
-	while (*(str + i))
+	while (*(str + len))
+		len++;
+	while (i < len)
 	{
 		_putchar(*(str + i));
 		i += 2;
 	}
 	_putchar('\n');
+}
+int main(void)
+{
+	char *str;
+
+	str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\0Holberton";
+	puts2(str);
+	return (0);
 }

0b76d2a40215dff71238bf3ce40f0c76c940a5a4
Anwar <anwar.28.8.1999@outlook.com>
2023-10-17 03:17:26 +0300

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * print_rev - print string in reverse.
+  * @s: the string to be reversed.
+  *
+  * Retrun: Nothing.
+  */
+
+void print_rev(char *s)
+{
+	int len = 0;
+	char *c = s;
+
+	while (*c)
+	{
+		len++;
+		c++;
+	}
+
+	while (len >= 0)
+	{
+		_putchar(*(s + len - 1));
+		len--;
+	}
+	_putchar ('\n');
+}

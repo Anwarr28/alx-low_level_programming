c9d1d36fcbdf9e3230eba078e1655f5580a58915
Anwar <anwar.28.8.1999@outlook.com>
2023-10-19 08:37:59 +0300

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  * leet - capitalize all words of a stirng.
+  * @a: a pointer to the stirng to be encoded.
+  *
+  * Return: a pointer to the new string.
+  */
+
+char *leet(char *a)
+{
+	char leet[] = "AaEeOoTtLl";
+	int i, j;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		for (j = 0; del[j] != '\0'; j++)
+		{
+			if (a[i] == leet[j])
+			{
+				if (leet[j] == 'a' || leet[j] == 'A')
+					a[i] = '4';
+				if (leet[j] == 'e' || leet[j] == 'E')
+					a[i] = '3';
+				if (leet[j] == 'o' || leet[j] == 'O')
+					a[i] = '0';
+				if (leet[j] == 't' || leet[j] == 'T')
+					a[i] = '7';
+				if (leet[j] == 'l' || leet[j] == 'L')
+					a[i] = '1';
+			}
+		}
+	}
+	return (a);
+}

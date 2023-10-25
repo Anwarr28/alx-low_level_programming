3f42c53a63281dd7ab73a013797b21bfa0bc22be
Anwar <anwar.28.8.1999@outlook.com>
2023-10-25 05:25:26 +0300

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+  * _strstr - locates a substring.
+  * @haystack: the main string to be scaned..
+  * @needle: the substring to be located.
+  *
+  * Return: a pointer to the beginning of the locatedsubstring in the main one.
+  */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	while (*haystack)
+	{
+		if (*haystack == needle[0])
+			break;
+		haystack++;
+	}
+	if (*haystack == '\0')
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i == 0; needle[i] != '\0'; i++)
+		{
+			if (needle[i] == haystack[i])
+				continue;
+			else
+				break;
+		}
+		if (needle[i] == '\0')
+			return (haystack);
+		else
+			return (NULL);
+	}
+
+}

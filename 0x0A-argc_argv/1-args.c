2c946a27177fb6a09507b6a4b7cf627eeba48db7
Anwar <anwar.28.8.1999@outlook.com>
2023-10-28 03:47:41 +0200

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * main - Entry point.
+  * @argc: the number of arguments.
+  * @argv: the names of arguments.
+  *
+  * Description: pint number of arguments.
+  * Return: 0 success.
+  */
+int main(int argc, char **argv)
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}

6d7ae221972bf1281b419e7340eaec4d42ca3854
Anwar <anwar.28.8.1999@outlook.com>
2023-10-28 04:38:26 +0200

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  * main - Entry point.
+  * @argc: the number of arguments.
+  * @argv: the names of arguments.
+  *
+  * Description: add positive numbers.
+  * Return: 0 success.
+  */
+int main(int argc, char **argv)
+{
+	int sum = 0, i, j;
+
+	if (argc == 1)
+		printf("0\n");
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (isdigit(argv[i][j]) != 0)
+					sum += atoi(argv[i]);
+				else
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}

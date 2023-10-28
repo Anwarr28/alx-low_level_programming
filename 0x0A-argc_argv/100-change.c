d9c6505429f26e26cecaa09fe49891334ca4b23c
Anwar <anwar.28.8.1999@outlook.com>
2023-10-28 05:55:41 +0200

    task 100 adv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,21 +12,26 @@
 int main(int argc, char **argv)
 {
 	int i, change = 0, coin[] = {25, 10, 5, 2, 1};
-	int money = atoi(argv[1]);
+	int money;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	money = atoi(argv[1]);
 	if (money < 0)
-		printf("0\n");
-
-	for (i = 0; money != 0; i++)
 	{
-		change += money / coin[i];
-		money = money % coin[i];
+		printf("0\n");
 	}
-	printf("%d\n", change);
+	else
+	{
+		for (i = 0; money != 0; i++)
+		{
+			change += money / coin[i];
+			money = money % coin[i];
+		}
+		printf("%d\n", change);
+	}
 	return (0);
 }

a88a67d0f5154eff4575ecf3fe773600d5d89d6e
Anwar <anwar.28.8.1999@outlook.com>
2023-10-28 03:34:44 +0200

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * main - Entry point.
+  * @argc: the number of arguments.
+  * @argv: the arguments names.
+  *
+  * Description: program prints its name.
+  * Return: 0 (Success).
+  */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

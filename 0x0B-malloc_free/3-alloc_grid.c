2eaacf53d2705e718bb495dd49491eb3eb217577
Anwar <anwar.28.8.1999@outlook.com>
2023-11-02 20:32:57 +0200

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	arr = (int *) malloc(sizeof(int) * (width * height));
+	arr = (int **) malloc(sizeof(int) * (width * height));
 	if (arr == NULL)
 		return (NULL);
 

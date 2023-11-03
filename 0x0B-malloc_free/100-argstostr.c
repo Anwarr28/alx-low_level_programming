ab08ff8efaf483bdad49b28da96981a6e64e014e
Anwar <anwar.28.8.1999@outlook.com>
2023-11-03 18:02:25 +0200

    task 100 adv

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * argstostr - concatenates all the aarguments of your program.
+  * @ac: the number of args.
+  * @av: an array of the args.
+  *
+  * Return: a pointer to a new string or a null if fails.
+  */
+char *argstostr(int ac, char **av)
+{
+	char *s;
+	int i, j, size = 0;
+
+	if (ac == 0)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			size++;
+	}
+	size = size + ac + 1;
+	s = (char *) realloc(s, sizeof(char) * size)
+	if (s == NULL)
+		return (NULL);
+
+	return (s);
+}

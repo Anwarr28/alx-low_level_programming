23f118d5ea56d6a86e31fe8ff84485470fe239c2
Anwar <anwar.28.8.1999@outlook.com>
2023-11-04 21:44:08 +0200

    task 0

---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,3 @@
+learning more about allocating memory in the heap
+using malloc, calloc and realloc.
+and of course deallocating it using free
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MIAN_H
+#define MIAN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void *malloc_checked(unsigned int b);
+
+
+#endif

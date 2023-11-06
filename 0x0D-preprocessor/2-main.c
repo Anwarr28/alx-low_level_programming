a9106ccfc249989c48e44b4ff71846eff2b1f647
Anwar <anwar.28.8.1999@outlook.com>
2023-11-06 19:25:13 +0200

    tasks 2-4

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point.
+  *
+  * Description: print the name of this file name.
+  * Return: always 0 success.
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}

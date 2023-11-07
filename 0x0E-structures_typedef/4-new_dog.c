3f80ef68d1c086dc08a417ae5ff2673931cf3247
Anwar <anwar.28.8.1999@outlook.com>
2023-11-07 03:06:11 +0200

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,7 +11,9 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t newDog;
-	dog_t *p = &newDog;
+	dog_t *p = malloc(sizeof(newDog));
+	if (p == NULL)
+		return (NULL);
 
 	p->name = name;
 	p->age = age;

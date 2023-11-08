0c5314e414cd262e2680ffe110d4cc508f357a89
Anwar <anwar.28.8.1999@outlook.com>
2023-11-09 01:20:53 +0200

    header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MIAN_H
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,4 +7,4 @@
 
 void print_name(char *name, void (*f)(char *));
 
-#endif
+#endif /* FUNCTION_POINTER_H */

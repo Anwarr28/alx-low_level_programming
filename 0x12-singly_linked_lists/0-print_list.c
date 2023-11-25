487cf43c1941d1861818a3d56d8b48ee968e9727
Anwar <anwar.28.8.1999@outlook.com>
2023-11-25 17:48:03 +0200

    edit betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 
 /**
   * print_list - prints all the elements of a list_t list.
-  * h: a structure pointer to the list.
+  * @h: a structure pointer to the list.
   *
   * Return: the number of nodes.
   */

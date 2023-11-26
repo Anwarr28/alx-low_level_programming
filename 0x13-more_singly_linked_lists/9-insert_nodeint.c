73976316b6e9241b9e723ec337fc37e3520b7807
Anwar <anwar.28.8.1999@outlook.com>
2023-11-27 00:26:00 +0200

    task 9 for the thousand time

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,35 +13,32 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *temp1 = NULL;
 	listint_t *temp2 = NULL;
-	unsigned int i = 0;
 
 	if (head == NULL)
 		return (NULL);
 
-	temp1 = *head;
-	while (temp1 != NULL)
-	{
-		temp1 = temp1->next;
-		i++;
-	}
-	if (idx >= i)
-		return (NULL);
+	temp2 = *head;
 
 	temp1 = malloc(sizeof(listint_t));
 	if (temp1 == NULL)
 		return (NULL);
+
 	temp1->n = n;
 	temp1->next = NULL;
 
-	temp2 = *head;
 	if (idx == 0)
 	{
-		temp1->next = temp2;
+		temp1->next = *head;
 		*head = temp1;
 		return (temp1);
 	}
-	while (--idx > 0)
+	while (--idx > 0 && temp2 != NULL)
 		temp2 = temp2->next;
+	if (temp2 == NULL)
+	{
+		free(temp1);
+		return (NULL);
+	}
 	temp1->next = temp2->next;
 	temp2->next = temp1;
 

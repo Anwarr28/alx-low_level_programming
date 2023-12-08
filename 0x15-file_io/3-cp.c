c747f5cf02354f3ebba23ac61ebbca87e1671644
Anwar <anwar.28.8.1999@outlook.com>
2023-12-08 11:45:47 +0200

    task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+/**
+  * main - program that copies the content of a file to another file.
+  * @argc: the number of argument passed to the program.
+  * @argv: an array of strings holding the arguments passed.
+  *
+  * Return: 0 if prog successeded,
+  *		otherwise the prog will exit with an error message.
+  */
+int main(int argc, char *argv[])
+{
+	int fd1, fd2;
+	ssize_t bytRd = 1, bytWr;
+	char buf[BUFFER_SIZE];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd2 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 664);
+	if (fd2 < 0)
+	{
+		close(fd1);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	while (bytRd > 0)
+	{
+		bytRd = read(fd1, buf, BUFFER_SIZE);
+		if (bytRd < 0)
+		{
+			close(fd1);
+			close(fd2);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		bytWr = write(fd2, buf, bytRd);
+		if (bytWr < 0)
+		{
+			close(fd1);
+			close(fd2);
+			dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", argv[2]);
+			exit(99);
+		}
+
+	}
+	if (close(fd1) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	if (close(fd2) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+	return (0);
+}
